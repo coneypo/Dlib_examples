c1af2cc70cb51c5f2520fb512c55e4395baf883a
root <coneypo@foxmail.com>
2019-01-28 07:57:51 +0000

    doc: update README

---
+++ README.rst
@@ -0,0 +1,43 @@
+Dlib examples
+#############
+
+Introduction
+************
+
+The examples of using Dlib in Python
+
+
+Face detection
+**************
+
+#. face_detector.py
+
+   The origin python code
+
+#. face_detector_v1.py
+
+   Add comments
+
+#. face_detector_v2_use_opencv.py
+
+   Using opencv object
+
+
+Face landmark detection
+***********************
+
+#. face_landmark_detection.py
+
+   The origin python code
+
+#. face_landmark_detection_v1.py
+
+   Add some comments
+
+
+More
+****
+
+Author: coneypo
+
+Thanks for your support.
--- face_landmark_detection/face_landmark_detection_v1.py
+++ face_landmark_detection/face_landmark_detection_v1.py
@@ -1,5 +1,5 @@
 # created at 2017-11-27
-# updated at 2018-09-06
+# updated at 2019-01-28
 
 # Author:   coneypo
 # Dlib:     http://dlib.net/
@@ -12,17 +12,17 @@ from skimage import io
 # 使用 Dlib 的正面人脸检测器 frontal_face_detector
 detector = dlib.get_frontal_face_detector()
 
-# Dlib 的 68点模型
-predictor = dlib.shape_predictor("../data/data_faces/shape_predictor_68_face_landmarks.dat")
+# Dlib 的 68 点模型
+predictor = dlib.shape_predictor("../data/data_dlib/shape_predictor_68_face_landmarks.dat")
 
 # 图片所在路径
-img = io.imread("../data/faces_2.jpeg")
+img = io.imread("../data/data_faces/faces_2.jpeg")
 
 # 生成 Dlib 的图像窗口
 win = dlib.image_window()
 win.set_image(img)
 
-# 使用 detector 检测器来检测图像中的人脸
+# 使用检测器来检测图像中的人脸
 faces = detector(img, 1)
 print("人脸数：", len(faces))
 
@@ -32,7 +32,8 @@ for i, d in enumerate(faces):
 
     # 使用 predictor 来计算面部轮廓
     shape = predictor(img, faces[i])
-    # 绘制面部轮廓 , Blue
+
+    # 绘制面部轮廓, Blue
     # full_object_detection
     win.add_overlay(shape)
 
